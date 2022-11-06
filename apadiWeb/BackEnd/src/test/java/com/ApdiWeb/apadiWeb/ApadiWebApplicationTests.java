95e325d12c7413237198ba2c8ea0058b003e093d
KDmiyanigit <113465875+KDmiyanigit@users.noreply.github.com>
2022-11-05 21:29:52 -0400

    Add files via upload
    first upload

---
+++ apadiWeb/BackEnd/src/test/java/com/ApdiWeb/apadiWeb/ApadiWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ApdiWeb.apadiWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApadiWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
